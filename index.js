5bf985a53e95f5d359b235293c5677b236c1028f
Lucia Velasco <luciakvelasco@gmail.com>
2020-10-26 15:21:21 +0000

    Scales index and tests to have multiple functions to test at once

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 
-module.exports = function() { return new Error("No code written! Check README.md for instructions.") }
+module.exports = {
+    exampleFunction: function() { return new Error("No code written! Check README.md for instructions.") }
+}
--- tests.js
+++ tests.js
@@ -1,53 +1,62 @@
-const getWordLengths = require("./index.js")
+const functionsToTest = require("./index.js")
 
-let count;
+const runTest = (getWordLengths, functionName) => {
 
-console.log("getWordLengths returns short, medium and long words for a single sentence.")
-count = getWordLengths("What a great day for an enormous picnic in my garden")
+    let count;
 
-if (typeof count !== 'object') throw new Error("Invalid return value. Must be object.")
+    console.log(`${functionName} returns short, medium and long words for a single sentence.`)
+    count = getWordLengths("What a great day for an enormous picnic in my garden")
 
-"short" in count &&
-"medium" in count &&
-"long" in count &&
-count.short === 6 && count.medium === 2 && count.long === 3 ?
-    console.log("PASS") :
-    console.warn("FAIL")
-/*
-  {
-  short: 6,
-  medium: 2,
-  long: 3
-  }
- */
+    if (typeof count !== 'object') throw new Error("Invalid return value. Must be object.")
 
-
-console.log("getWordLengths does not return categories with count of 0")
-count = getWordLengths("Palaver over nothing")
-"short" in count === false &&
-"medium" in count &&
-"long" in count &&
-count.medium === 1 && count.long === 2 ?
-    console.log("PASS") :
-    console.warn("FAIL")
-/*
-  {
-  medium: 1,
-  long: 2
-  }
- */
+    "short" in count &&
+    "medium" in count &&
+    "long" in count &&
+    count.short === 6 && count.medium === 2 && count.long === 3 ?
+        console.log("PASS") :
+        console.warn("FAIL")
+    /*
+      {
+      short: 6,
+      medium: 2,
+      long: 3
+      }
+     */
 
 
-console.log("getWordLengths ignores punctuation when counting lengths")
-count = getWordLengths("Hello! Hello!")
-"short" in count === false &&
-"medium" in count &&
-"long" in count === false &&
-count.medium === 2 ?
-    console.log("PASS") :
-    console.warn("FAIL")
-/*
-  {
-  medium: 2
-  }
- */
+    console.log(`${functionName} does not return categories with count of 0`)
+    count = getWordLengths("Palaver over nothing")
+    "short" in count === false &&
+    "medium" in count &&
+    "long" in count &&
+    count.medium === 1 && count.long === 2 ?
+        console.log("PASS") :
+        console.warn("FAIL")
+    /*
+      {
+      medium: 1,
+      long: 2
+      }
+     */
+
+
+    console.log(`${functionName} ignores punctuation when counting lengths`)
+    count = getWordLengths("Hello! Hello!")
+    "short" in count === false &&
+    "medium" in count &&
+    "long" in count === false &&
+    count.medium === 2 ?
+        console.log("PASS") :
+        console.warn("FAIL")
+    /*
+      {
+      medium: 2
+      }
+     */
+}
+
+const keys = Object.keys(functionsToTest)
+keys.forEach(functionName => {
+    console.log('TESTING:', functionName, '\n')
+    runTest(functionsToTest[functionName], functionName)
+})
