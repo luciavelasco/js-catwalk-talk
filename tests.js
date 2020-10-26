1f6892fa65d11d4350c29485906f8c0d27e30297
Lucia Velasco <luciakvelasco@gmail.com>
2020-10-26 16:31:13 +0000

    Add exercise numbers to test output

--- tests.js
+++ tests.js
@@ -3,15 +3,15 @@ const functionsToTest = require("./index.js")
 const runTest = (getWordLengths, functionName) => {
     let count;
 
-    console.log(`${functionName} returns short, medium and long words for a single sentence.`)
+    console.log(`EXERCISE 1: ${functionName} returns short, medium and long words for a single sentence.`)
     count = getWordLengths("What a great day for an enormous picnic in my garden")
     if (typeof count !== 'object') throw new Error("Invalid return value. Must be object.")
     "short" in count &&
     "medium" in count &&
     "long" in count &&
     count.short === 6 && count.medium === 2 && count.long === 3 ?
-        console.log("PASS") :
-        console.warn("FAIL")
+        console.log("EXERCISE 1: PASS") :
+        console.warn("EXERCISE 1: FAIL")
     /*
       {
       short: 6,
@@ -21,14 +21,14 @@ const runTest = (getWordLengths, functionName) => {
      */
 
 
-    console.log(`${functionName} does not return categories with count of 0`)
+    console.log(`EXERCISE 2: ${functionName} does not return categories with count of 0`)
     count = getWordLengths("Palaver over nothing")
     "short" in count === false &&
     "medium" in count &&
     "long" in count &&
     count.medium === 1 && count.long === 2 ?
-        console.log("PASS") :
-        console.warn("FAIL")
+        console.log("EXERCISE 2: PASS") :
+        console.warn("EXERCISE 2: FAIL")
     /*
       {
       medium: 1,
@@ -37,14 +37,14 @@ const runTest = (getWordLengths, functionName) => {
      */
 
 
-    console.log(`${functionName} ignores punctuation when counting lengths`)
+    console.log(`EXERCISE 3: ${functionName} ignores punctuation when counting lengths`)
     count = getWordLengths("Hello! Hello!")
     "short" in count === false &&
     "medium" in count &&
     "long" in count === false &&
     count.medium === 2 ?
-        console.log("PASS") :
-        console.warn("FAIL")
+        console.log("EXERCISE 3: PASS") :
+        console.warn("EXERCISE 3: FAIL")
     /*
       {
       medium: 2
