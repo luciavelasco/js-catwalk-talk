dc0980de51286373d6fb33f9e820e889292a767f
Lucia Velasco <luciakvelasco@gmail.com>
2024-03-13 17:09:37 +0000

    Add Cat, Mike and Lucia's solutions

--- index.js
+++ index.js
@@ -1,4 +1,72 @@
+// Mike's Solution
+const increment = (output, key) => {
+    output[key] = ++output[key] || 1
+    // output[key] = (output.hasOwnProperty(key) ? output[key]++ : 1)
+    return output
+}
+
+const mikeFunction = (words) => {
+    words = words.split(' ')
+    let output = {}
+    words.forEach(word => {
+        if (word.length <= 3) {
+            output = increment(output, 'short')
+        } else if (word.length <= 5) {
+            output = increment(output, 'medium')
+        } else {
+            output = increment(output, 'long')
+        }
+    })
+    return output
+}
+
+
+// Cat's Solution
+// catFunction goes here...
+function catFunction(words) {
+  let splitWords = words.split(' ')
+  let wordsLessThree = splitWords.filter((word) => word.length <= 3)
+  let wordsThreeToFive = splitWords.filter((word) => word.length > 3 && word.length <= 5)
+  let wordsMoreFive = splitWords.filter((word) => word.length > 5)
+
+  let output = {}
+
+  if (wordsLessThree.length > 0) {
+    output.short = wordsLessThree.length
+  }
+  if (wordsThreeToFive.length > 0) {
+    output.medium = wordsThreeToFive.length
+  }
+  if (wordsMoreFive.length > 0) {
+    output.long = wordsMoreFive.length
+  }
+  
+  return output;
+}
+
+// Lucia's Solution
+// luciaFunction goes here...
+const getCategory = word =>
+  word.length <= 3 ? `short` :
+  word.length <= 5 ? `medium` :
+  `long`
+
+const luciaFunction = sentence =>
+  sentence
+    .split(` `)
+    .map(getCategory)
+    .reduce(
+      (accumulator, category) => ({
+        ...accumulator,
+        [category]: accumulator[category] + 1 || 1
+      }),
+      { }
+    )
+
 
 module.exports = {
-    exampleFunction: function(sentence) { return new Error("No code written! Check README.md for instructions.") },
+  mikeFunction,
+  catFunction,
+  luciaFunction
 }
+
